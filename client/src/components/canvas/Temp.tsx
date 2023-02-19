d4c05f24d92df3759c41a8e96f39bb3ad79a4e22
John Beresford <jberesford@volcaus.com>
2023-02-19 15:13:44 -0800

    chore(client): fix minor nits
    - remove lock file from .gitignore
    - add .glsl files to TS module declaration

--- client/src/components/canvas/Temp.tsx
+++ client/src/components/canvas/Temp.tsx
@@ -12,6 +12,7 @@ export default function Temp() {
   return (
     <>
       <OrbitControls enableZoom={false} />
+
       <mesh ref={ref}>
         <boxBufferGeometry args={[1, 1, 1]} />
         <meshNormalMaterial />
