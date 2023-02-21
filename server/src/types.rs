642715e070aeb332868fb96cf5c2e6c323dc7dd2
John Beresford <jberesford@volcaus.com>
2023-02-20 21:37:09 -0800

    server POC init

--- server/src/types.rs
+++ server/src/types.rs
@@ -1,14 +1,31 @@
 use std::collections::HashMap;
 
+#[derive(Clone, Copy)]
+pub struct Position {
+    pub x: f32,
+    pub y: f32,
+    pub z: f32,
+}
+
+#[derive(Clone, Copy)]
 pub struct Entity {
-    pub id: i32,
-    pub pos: (i32, i32),
+    pub id: u64,
+    pub pos: Position,
+}
+
+impl Position {
+    pub fn to_json(&self) -> String {
+        format!("{{\"x\":{},\"y\":{}, \"z\":{}}}", self.x, self.y, self.z)
+    }
 }
 
 impl Entity {
     pub fn to_json(&self) -> String {
-        format!("{{\"position\":{{\"x\":{},\"y\":{}}}, \"id\":{}}}", self.pos.0, self.pos.1, self.id)
+        format!(
+            "{{\"position\":{{\"x\":{},\"y\":{}, \"z\":{}}}, \"id\":{}}}",
+            self.pos.x, self.pos.y, self.pos.z, self.id
+        )
     }
 }
 
-pub type Entities = HashMap<u32, Entity>;
+pub type Entities = HashMap<u64, Entity>;
