5d28ef51fac84620bf3723ec37f49912aaadddfa
Christopher Sterza <ChristopherSterza@gmail.com>
2023-03-01 16:06:33 -0800

    Added supabase Context

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,23 +1,28 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import Header from "@/config";
 import "@/styles/global.scss";
 import styles from "@/styles/Layout.module.scss";
 import dynamic from "next/dynamic";
+import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs";
+import { SessionContextProvider, Session } from "@supabase/auth-helpers-react";
 
 const Scene = dynamic(() => import("@/components/canvas/Scene"), { ssr: true });
 
-export default function App({ Component, pageProps = { title: "index" } }) {
+type Props = { Component: any; pageProps: { title: string; initialSession: Session } };
+export default function App({ Component, pageProps }: Props) {
+  const [supabaseClient] = useState(() => createBrowserSupabaseClient());
   const ref = useRef();
   return (
-    <>
+    <SessionContextProvider
+      supabaseClient={supabaseClient}
+      initialSession={pageProps.initialSession}
+    >
       <Header title={pageProps.title} />
       <div ref={ref} className={styles.domElem}>
         <Component {...pageProps} />
-        {/* The canvas can either be in front of the dom or behind. If it is in front it can overlay contents.
-         * Setting the event source to a shared parent allows both the dom and the canvas to receive events.
-         * Since the event source is now shared, the canvas would block events, we prevent that with pointerEvents: none. */}
+
         {Component?.canvas && <Scene eventSource={ref}>{Component.canvas(pageProps)}</Scene>}
       </div>
-    </>
+    </SessionContextProvider>
   );
 }
